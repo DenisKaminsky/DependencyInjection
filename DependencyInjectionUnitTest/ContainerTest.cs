764e27ae708e46cbf4adade65355a3e2126ed556
DenisKaminsky <deniskaminsky123@mail.ru>
2018-12-11 14:58:46 +0300

    dependency test

--- DependencyInjectionUnitTest/ContainerTest.cs
+++ DependencyInjectionUnitTest/ContainerTest.cs
@@ -9,6 +9,20 @@ namespace DependencyInjectionUnitTest
     public class ContainerTest
     {
         [TestMethod]
+        public void CreateDependencyTest()
+        {
+            DependencyProvider provider;
+            DependenciesConfiguration config = new DependenciesConfiguration();
+
+            config.Register<IExample, ClassForExample>(false);
+            config.Register<IExample, ClassForExample2>(true);
+            provider = new DependencyProvider(config);
+            IExample actual = provider.Resolve<IExample>();
+
+            Assert.IsNotNull(actual);
+        }
+
+        [TestMethod]
         public void SingletonTest()
         {
             DependencyProvider provider;
@@ -34,18 +48,19 @@ namespace DependencyInjectionUnitTest
             IExample actual = provider.Resolve<IExample>();
 
             Assert.AreNotEqual(expected, actual);
-        }
-
-        [TestMethod]
-        public void CreateDependencyTest()
-        {
-
-        }
+        }      
 
         [TestMethod]
         public void AsSelfRegistrationTest()
         {
+            DependencyProvider provider;
+            DependenciesConfiguration config = new DependenciesConfiguration();
 
+            config.Register<ClassForExample, ClassForExample>(true);
+            provider = new DependencyProvider(config);
+            ClassForExample actual = provider.Resolve<ClassForExample>();
+
+            Assert.IsNotNull(actual);
         }
     }
 }
